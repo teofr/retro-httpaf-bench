190218dfb2d73a4b309ce6c28e031d1bd17f673d
Teodoro Freund <tfreund95@gmail.com>
2023-11-13 10:19:57 +0000

    pinned eio dep

--- cohttp-eio/build.sh
+++ cohttp-eio/build.sh
@@ -5,7 +5,7 @@ set -xe
 
 opam switch remove --yes cohttp-eio-build || true
 rm -f ./cohttp_eio.exe || true
-opam switch create --packages=dune,eio_main,mdx,uri,fmt,ocaml.5.1.0,ptime cohttp-eio-build
+opam switch create --packages=dune,eio_main.0.11,mdx,uri,fmt,ocaml.5.1.0,ptime cohttp-eio-build
 opam exec -- dune clean --profile=release
 opam exec -- dune build --profile=release
 cp ./_build/default/cohttp_eio.exe ./cohttp_eio.exe
